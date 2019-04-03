dcfe74b0cc692a7f065ed4416dcf15c3a558c1b1
Jack Kamm <jackkamm@gmail.com>
2019-04-03 16:04:43 -0700

    Refactor contamarkov()

--- contamarkov.R
+++ contamarkov.R
@@ -1,59 +1,78 @@
 library(magrittr)
 
-contamarkov <- function(sample_table, reports) {
+contamarkov <- function(sample_table, reports, fdr_threshold=.1) {
   sample_table %>%
     dplyr::mutate(host_reads=total_reads-nonhost_reads-total_ercc_reads) %>%
-    dplyr::mutate(host_mass=host_reads/total_ercc_reads,
-                  nonhost_mass=nonhost_reads/total_ercc_reads) ->
+    dplyr::mutate(host_concentration=host_reads/total_ercc_reads,
+                  nonhost_concentration=nonhost_reads/total_ercc_reads) %>%
+    dplyr::mutate(total_sample_concentration=(total_reads-total_ercc_reads) / total_ercc_reads *
+                    ercc_concentration)->
     sample_table
 
   sample_table %>%
     dplyr::filter(!is_water) %>%
-    with(sum(host_mass)) ->
+    with(sum(host_concentration)) ->
     total_host_mass
 
   sample_table %>%
     dplyr::filter(is_water) %>%
-    with(mean(host_mass)) / total_host_mass ->
+    with(mean(host_concentration)) / total_host_mass ->
     mean_spill_frac
 
   reports %>%
     dplyr::inner_join(sample_table) %>%
     dplyr::filter(!is_water) %>%
-    dplyr::group_by(name) %>%
-    dplyr::summarize(spillome_concentration=sum(NT_r / total_ercc_reads) *
-                       mean_spill_frac) ->
+    dplyr::group_by(tax_id) %>%
+    dplyr::summarize(spillome_concentration=sum(NT_r / total_ercc_reads * ercc_concentration) *
+                       mean_spill_frac,
+                     category_name=unique(category_name)) ->
     spillome
 
   n_water <- sum(sample_table$is_water)
   reports %>%
     dplyr::inner_join(sample_table) %>%
     dplyr::filter(is_water) %>%
-    dplyr::group_by(name) %>%
-    dplyr::summarize(labome_concentration=sum(NT_r / total_ercc_reads) / n_water) ->
+    dplyr::group_by(tax_id) %>%
+    dplyr::summarize(labome_concentration=sum(NT_r / total_ercc_reads * ercc_concentration) / n_water, category_name=unique(category_name)) ->
     labome
 
   dplyr::full_join(spillome, labome) %>%
     tidyr::replace_na(list(spillome_concentration=0, labome_concentration=0)) %>%
-    #dplyr::mutate(mass_mean=pmax(spillome_concentration, labome_concentration)) ->
-    dplyr::mutate(mass_mean=spillome_concentration + labome_concentration) ->
+    dplyr::mutate(contaminome_concentration=spillome_concentration + labome_concentration) ->
     contaminome
 
   reports %>%
     dplyr::inner_join(sample_table) %>%
-    dplyr::mutate(mass=NT_r / total_ercc_reads) %>%
+    dplyr::mutate(tax_concentration=NT_r / total_ercc_reads * ercc_concentration) %>%
     dplyr::left_join(contaminome) %>%
-    dplyr::mutate(mass_corrected=mass-mass_mean) %>%
-    dplyr::mutate(pval=pmin(1, mass_mean / mass)) %>%
+    dplyr::mutate(pval=pmin(1, contaminome_concentration / tax_concentration)) %>%
     dplyr::group_by(category_name) %>%
     dplyr::mutate(padj=p.adjust(pval, method="BH")) %>%
     dplyr::ungroup() %>%
-    dplyr::select(sample_name, name, NT_r, NT_rpm, category_name, total_ercc_reads,
-                  is_water, LRTI_adjudication,
-                  mass, mass_mean, mass_corrected,
-                  spillome_concentration, labome_concentration,
-                  pval, padj) ->
-    cleaned_reports
+    dplyr::select(!!! c(names(reports), "tax_concentration",
+                        "total_sample_concentration", "pval", "padj")) ->
+    reports
 
-  list(contaminome=contaminome, reports=cleaned_reports)
+  reports %>%
+    dplyr::filter(padj <= .1) %>%
+    dplyr::group_by(category_name) %>%
+    dplyr::summarize(threshold=max(pval)) ->
+    pval_thresholds
+
+  contaminome %>%
+    dplyr::rename(spillome=spillome_concentration, labome=labome_concentration) %>%
+    dplyr::select(tax_id, category_name, spillome, labome, contaminome_concentration) %>%
+    dplyr::inner_join(pval_thresholds) %>%
+    dplyr::mutate(threshold=contaminome_concentration/threshold) %>%
+    dplyr::select(-contaminome_concentration) %>%
+    dplyr::rename(cutoff=threshold) %>%
+    dplyr::rename(spillome_expected=spillome, labome_expected=labome) %>%
+    tidyr::gather(line, concentration, -tax_id, -category_name, -tax_id) %>%
+    dplyr::filter(concentration > 0) %>%
+    dplyr::mutate(log10_rpm_intercept=log10(concentration*1e6)) %>%
+    dplyr::select(tax_id, line, log10_rpm_intercept) ->
+    log10_rpm_intercept
+
+  list(log10_rpm_intercept=log10_rpm_intercept, reports=reports)
 }
+
