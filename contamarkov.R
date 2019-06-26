15e3a7849f49afb20f0c1d0cdc65e613ab26de13
Jack Kamm <jackkamm@gmail.com>
2019-06-26 16:37:44 -0700

    Switch to using reads_pct, correctly account for subsampled_fraction

--- contamarkov.R
+++ contamarkov.R
@@ -2,44 +2,55 @@ library(magrittr)
 library(ggplot2)
 
 contamarkov <- function(sample_table, reports, fdr_threshold=.1,
-                        r_column="NT_r", rpm_column="NT_rpm") {
+                        use_nr=FALSE) {
   reports %>%
-    dplyr::mutate(n_reads=!!sym(r_column), RPM=!!sym(rpm_column)) %>%
-    dplyr::inner_join(dplyr::select(sample_table, sample_name,
-                                    nonhost_reads, compression_ratio)) %>%
-    dplyr::mutate(n_reads = compression_ratio * n_reads,
-                  RPM = compression_ratio * RPM) %>%
-    dplyr::select(-compression_ratio, -nonhost_reads) ->
+    dplyr::mutate(n_reads=if (use_nr) NR_r else NT_r) %>%
+    {if (use_nr)
+       dplyr::mutate(., n_reads=NR_r, reads_pct2=NR_r_pct)
+      else
+        dplyr::mutate(., n_reads=NT_r, reads_pct2=NT_r_pct)} %>%
+    dplyr::inner_join(
+      dplyr::select(sample_table, sample_name,
+                    subsampled_fraction, nonhost_reads)
+    ) %>%
+    dplyr::mutate(reads_pct=NT_r / subsampled_fraction / nonhost_reads * 100) %>%
+    dplyr::select(-subsampled_fraction, -nonhost_reads) ->
     reports
 
+  reports %>%
+    dplyr::filter(!is.na(reads_pct2)) %>%
+    dplyr::arrange(desc(abs(log(reads_pct / reads_pct2)))) %>%
+    {all.equal(round(.$reads_pct, 5), .$reads_pct2)} %>%
+    stopifnot()
+
   sample_table %>%
-    dplyr::mutate(nonhost_reads = nonhost_reads * compression_ratio) %>%
-    dplyr::mutate(host_reads=total_reads-nonhost_reads*100/quality_control-total_ercc_reads) %>%
-    dplyr::mutate(host_concentration=host_reads/total_ercc_reads,
-                  nonhost_concentration=nonhost_reads/total_ercc_reads) %>%
-    dplyr::mutate(total_sample_concentration =
-                    (total_reads-total_ercc_reads) /
-                    total_ercc_reads *
-                    ercc_concentration) %>%
-    dplyr::filter(sample_table$sample_name %in% unique(reports$sample_name)) ->
+    dplyr::filter(sample_name %in% unique(reports$sample_name)) %>%
+    dplyr::mutate(
+      host_transcriptome_reads = total_reads - reads_after_star - total_ercc_reads) %>%
+    dplyr::mutate(
+      host_transcriptome_concentration=(host_transcriptome_reads *
+                                          ercc_concentration / total_ercc_reads),
+      nonhost_concentration=(nonhost_reads * compression_ratio *
+                               ercc_concentration / total_ercc_reads)) ->
     sample_table
 
   sample_table %>%
     dplyr::filter(!is_water) %>%
-    with(sum(host_concentration)) ->
-    total_host_mass
+    with(sum(host_transcriptome_concentration)) ->
+    total_host_transcriptome_mass
 
   sample_table %>%
     dplyr::filter(is_water) %>%
-    with(mean(host_concentration)) / total_host_mass ->
+    with(mean(host_transcriptome_concentration)) / total_host_transcriptome_mass ->
     mean_spill_frac
 
   reports %>%
     dplyr::inner_join(sample_table) %>%
     dplyr::filter(!is_water) %>%
     dplyr::group_by(tax_id, name, category_name) %>%
-    dplyr::summarize(spillome_concentration=sum(n_reads / total_ercc_reads * ercc_concentration) *
-                       mean_spill_frac) %>%
+    dplyr::summarize(spillome_concentration=mean_spill_frac * sum(
+      n_reads * compression_ratio / subsampled_fraction *
+        ercc_concentration / total_ercc_reads)) %>%
     dplyr::ungroup() ->
     spillome
 
@@ -48,8 +59,9 @@ contamarkov <- function(sample_table, reports, fdr_threshold=.1,
     dplyr::inner_join(sample_table) %>%
     dplyr::filter(is_water) %>%
     dplyr::group_by(tax_id, name, category_name) %>%
-    dplyr::summarize(labome_concentration=sum(n_reads / total_ercc_reads * ercc_concentration) /
-                       n_water) ->
+    dplyr::summarize(labome_concentration=sum(
+      n_reads * compression_ratio / subsampled_fraction *
+        ercc_concentration / total_ercc_reads) / n_water) ->
     labome
 
   dplyr::full_join(spillome, labome) %>%
@@ -59,15 +71,17 @@ contamarkov <- function(sample_table, reports, fdr_threshold=.1,
 
   reports %>%
     dplyr::inner_join(sample_table) %>%
-    dplyr::mutate(tax_concentration=n_reads / total_ercc_reads * ercc_concentration) %>%
+    dplyr::mutate(tax_concentration=(
+      n_reads * compression_ratio / subsampled_fraction *
+        ercc_concentration / total_ercc_reads)) %>%
     dplyr::left_join(contaminome) %>%
     dplyr::mutate(pval=pmin(1, contaminome_concentration / tax_concentration)) %>%
     dplyr::group_by(category_name) %>%
     dplyr::mutate(padj=p.adjust(pval, method="BH")) %>%
     dplyr::ungroup() %>%
     dplyr::select(!!! c(names(reports), "is_water", "tax_concentration",
-                        "contaminome_concentration", "labome_concentration", "spillome_concentration",
-                        "total_sample_concentration", "pval", "padj")) ->
+                        "contaminome_concentration", "labome_concentration", "spillome_concentration", "nonhost_concentration",
+                        "pval", "padj")) ->
     reports
 
   reports %>%
@@ -86,50 +100,29 @@ contamarkov <- function(sample_table, reports, fdr_threshold=.1,
     dplyr::rename(spillome_expected=spillome, labome_expected=labome) %>%
     tidyr::gather(line, concentration, -tax_id, -name, -category_name, -tax_id) %>%
     dplyr::filter(concentration > 0) %>%
-    dplyr::mutate(log10_rpm_intercept=log10(concentration*1e6)) %>%
-    dplyr::select(tax_id, name, line, log10_rpm_intercept) ->
-    log10_rpm_intercept
+    dplyr::mutate(log10_pct_intercept=log10(concentration*100)) %>%
+    dplyr::select(tax_id, name, line, log10_pct_intercept) ->
+    log10_pct_intercept
 
-  list(log10_rpm_intercept=log10_rpm_intercept, reports=reports)
+  list(log10_pct_intercept=log10_pct_intercept, reports=reports)
 }
 
-contamarkov_ggplot <- function(reports, log10_rpm_intercept, ...) {
+contamarkov_ggplot <- function(reports, log10_pct_intercept, ...) {
   if (is.factor(reports$name)) {
-    log10_rpm_intercept %>%
+    log10_pct_intercept %>%
       dplyr::mutate(name=as.character(name)) %>%
       dplyr::filter(name %in% unique(as.character(reports$name))) %>%
       dplyr::mutate(name=factor(name, levels=levels(reports$name))) ->
-      log10_rpm_intercept
+      log10_pct_intercept
   }
 
-  # filter log10_rpm_intercept to have same tax_id, name as reports
+  # filter log10_pct_intercept to have same tax_id, name as reports
   reports %>%
     dplyr::select(tax_id, name) %>%
     dplyr::distinct() %>%
-    dplyr::inner_join(log10_rpm_intercept) ->
-    log10_rpm_intercept
+    dplyr::inner_join(log10_pct_intercept) ->
+    log10_pct_intercept
 
-  ggplot(reports, aes(x=log10(total_sample_concentration), y=log10(RPM), ...)) +
-    geom_abline(data=log10_rpm_intercept, mapping=aes(slope=-1, intercept=log10_rpm_intercept, lty=line))
-}
-
-plot_contamarkov <- function(contamarkov_list, subset_taxa=NULL, point_aes=aes(color=is_water)) {
-  log10_rpm_intercept <- contamarkov_list$log10_rpm_intercept
-  reports <- contamarkov_list$reports
-
-  if (!is.null(subset_taxa)) {
-    log10_rpm_intercept %>%
-      dplyr::filter(tax_id %in% subset_taxa) ->
-      log10_rpm_intercept
-
-    reports %>%
-      dplyr::filter(tax_id %in% subset_taxa) ->
-      reports
-  }
-
-  ggplot(reports, mapping=aes(x=log10(total_sample_concentration), y=log10(RPM), group=tax_id)) +
-    geom_point(mapping=point_aes) +
-    geom_abline(data=log10_rpm_intercept,
-                mapping=aes(slope=-1, intercept=log10_rpm_intercept, lty=line)) +
-    facet_wrap(~name)
+  ggplot(reports, aes(x=log10(nonhost_concentration), y=log10(reads_pct), ...)) +
+    geom_abline(data=log10_pct_intercept, mapping=aes(slope=-1, intercept=log10_pct_intercept, lty=line))
 }
